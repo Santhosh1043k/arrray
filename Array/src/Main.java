00b0d625a7b2926be5664146279678b9f784aa1c
Santhosh1043k <159894976+Santhosh1043k@users.noreply.github.com>
2024-02-14 12:31:37 +0530

    Add files via upload

---
+++ Array/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int ar[] = new int[4];
+                ar[0] =3;
+                ar[1] =5;
+                ar[3] =8;
+                ar[4] 9;
+
+        //this is the one method of creating an array
+        int arr[] = {1,2,3};
+        //this is the another method of creating an array
+        System.out.println(arr[1]);
+    }
+}
